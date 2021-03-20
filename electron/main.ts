671834eb01492325055921cc8780c68e86a3c951
Gustavo Amaro <gustavoamaro@pop-os.localdomain>
2021-03-20 09:54:33 -0300

    remove unnecessary components

--- electron/main.ts
+++ electron/main.ts
@@ -7,8 +7,8 @@ let mainWindow: Electron.BrowserWindow | null
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 1100,
-    height: 700,
+    width: 800,
+    height: 600,
     backgroundColor: '#191622',
     webPreferences: {
       nodeIntegration: true
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { GlobalStyle } from './styles/GlobalStyle'
 
-import Greetings from './components/Greetings'
-
 const mainElement = document.createElement('div')
 mainElement.setAttribute('id', 'root')
 document.body.appendChild(mainElement)
@@ -12,7 +10,6 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Greetings />
     </>
   )
 }
